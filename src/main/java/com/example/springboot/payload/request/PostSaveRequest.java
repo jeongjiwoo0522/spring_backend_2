7d152454545b66d444a84ea5d3638236b78f9b7f
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-06-21 21:56:04 +0900

    [ADD] PostSaveRequestJava

---
+++ src/main/java/com/example/springboot/payload/request/PostSaveRequest.java
@@ -0,0 +1,26 @@
+package com.example.springboot.payload.request;
+
+import com.example.springboot.entity.post.Post;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostSaveRequest {
+
+    private String title;
+    private String content;
+    private String author;
+
+    public Post toEntity() {
+        return Post.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
