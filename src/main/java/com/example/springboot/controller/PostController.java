b83e528ef3b4a74bfbbb0d88e8c56599813f3478
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-06-21 22:30:55 +0900

    [MODIFY] PostService, PostController
    update post

--- src/main/java/com/example/springboot/controller/PostController.java
+++ src/main/java/com/example/springboot/controller/PostController.java
@@ -1,11 +1,10 @@
 package com.example.springboot.controller;
 
 import com.example.springboot.payload.request.PostSaveRequest;
+import com.example.springboot.payload.request.PostUpdateRequest;
 import com.example.springboot.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +15,10 @@ public class PostController {
     @PostMapping("/api/v1/post")
     public Long save(@RequestBody PostSaveRequest request) {
         return postService.save(request);
+    }
+
+    @PutMapping("/api/v1/post/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostUpdateRequest request) {
+        return postService.update(id, request);
     }
 }
--- src/main/java/com/example/springboot/service/PostService.java
+++ src/main/java/com/example/springboot/service/PostService.java
@@ -1,7 +1,9 @@
 package com.example.springboot.service;
 
+import com.example.springboot.entity.post.Post;
 import com.example.springboot.entity.post.PostRepository;
 import com.example.springboot.payload.request.PostSaveRequest;
+import com.example.springboot.payload.request.PostUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,5 +17,13 @@ public class PostService {
     @Transactional
     public Long save(PostSaveRequest request) {
         return postRepository.save(request.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostUpdateRequest request) {
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다. id=" + id));
+        post.update(request.getTitle(), request.getContent());
+        return id;
     }
 }
