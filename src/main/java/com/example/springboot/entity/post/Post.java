028efef825b26c6a9512caed523a357f68ecb8aa
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-06-22 09:46:20 +0900

    [ADD] BaseTimeEntity

--- src/main/java/com/example/springboot/entity/post/Post.java
+++ src/main/java/com/example/springboot/entity/post/Post.java
@@ -1,5 +1,6 @@
 package com.example.springboot.entity.post;
 
+import com.example.springboot.entity.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Post {
+public class Post extends BaseTimeEntity  {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
