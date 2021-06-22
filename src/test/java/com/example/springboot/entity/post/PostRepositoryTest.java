dfdd3a83a48b13ed1ca7c7c9e5c68024ef679cd9
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-06-22 09:55:22 +0900

    [MODIFY] PostRepositoryTest
    BaseTimeEntity 테스트

--- src/test/java/com/example/springboot/entity/post/PostRepositoryTest.java
+++ src/test/java/com/example/springboot/entity/post/PostRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @SpringBootTest
@@ -36,5 +37,29 @@ public class PostRepositoryTest {
         Post post = postList.get(0);
         assertThat(post.getTitle()).isEqualTo(title);
         assertThat(post.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    @Transactional
+    public void BaseTimeEntity_등록() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2021, 6, 4, 0, 0, 0);
+        postRepository.save(
+                Post.builder()
+                        .title("title")
+                        .content("content")
+                        .author("author")
+                        .build()
+        );
+        // when
+        List<Post> postList = postRepository.findAll();
+
+        // then
+        Post post = postList.get(0);
+
+        System.out.println(">>>>>>>>>>>>>> createdAt=" + post.getCreatedAt() + ", updatedAt=" + post.getUpdatedAt());
+
+        assertThat(post.getCreatedAt()).isAfter(now);
+        assertThat(post.getUpdatedAt()).isAfter(now);
     }
 }
