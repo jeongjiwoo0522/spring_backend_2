58df2356a865576f90af8609a777694228d34f19
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-06-27 17:31:18 +0900

    [MODIFY] IndexController
    LoginUser annotation

--- src/main/java/com/example/springboot/controller/IndexController.java
+++ src/main/java/com/example/springboot/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.springboot.controller;
 
+import com.example.springboot.config.auth.LoginUser;
 import com.example.springboot.config.auth.dto.SessionUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +15,7 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public SessionUser index() {
-        return (SessionUser) httpSession.getAttribute("user");
+    public SessionUser index(@LoginUser SessionUser user) {
+        return user;
     }
 }
